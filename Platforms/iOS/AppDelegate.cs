4f7eae8c8dbc0ea204ea2ff989ba8115912118af
Davidd0821 <126388661+Davidd0821@users.noreply.github.com>
2023-10-19 22:34:59 +0800

    Add files via upload

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Promee_Homepage;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
---
+++ PromeeNotification.xaml.cs
@@ -0,0 +1,9 @@
+namespace Promee_Homepage;
+
+public partial class xm : ContentPage
+{
+	public xm()
+	{
+		InitializeComponent();
+	}
+}
